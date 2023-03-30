b6727d60acfb45c0dacff720c5b5bd1534ebddc0
Aymen <aymenabduseid10@gmail.com>
2023-03-30 21:57:51 +0300

    update task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""
-function that finds a peak in a list of unsorted
-integers
-"""
+''' contains function find_peak '''
 
 
 def find_peak(list_of_integers):
-    """
-    function to find peak
-    """
-    l = len(list_of_integers)
-    if l == 0:
-        return
-    half = l // 2
-    if (half == l - 1 or list_of_integers[half] >=
-        list_of_integers[half + 1]) and (half == 0 or list_of_integers[half] >=
-                                         list_of_integers[half - 1]):
-        return (list_of_integers[half])
-    elif half != l - 1 and list_of_integers[half + 1] > list_of_integers[half]:
-        return (find_peak(list_of_integers[half + 1:]))
-    return (find_peak(list_of_integers[:half]))
+    ''' finds a peak in a list of unsorted integers '''
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    left = 0
+    right = len(list_of_integers) - 1
+    return peak_find(list_of_integers, left, right)
+
+
+def peak_find(list, left, right):
+    ''' finds peak recursively using binary search '''
+    if left == right:
+        return(list[right])
+    mid = (left + right) // 2
+    if list[mid] > list[mid + 1]:
+        return peak_find(list, left, mid)
+    return peak_find(list, mid + 1, right)
