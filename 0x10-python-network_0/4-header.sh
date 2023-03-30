73e2aeb439bfe83833fd27f9592d3df4b65ae893
Aymen <aymenabduseid10@gmail.com>
2023-03-30 22:01:08 +0300

    task4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends GET request to a URL and displays body of response
-curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Displays all HTTP methods the server will accept.
+curl -sX GET -H "X-School-User-Id: 98" "$*"
